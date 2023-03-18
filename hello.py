c28815c49610d2a245391ce3d694a53e6e92b3a8
Buntu William <williambuntu1@gmail.com>
2023-03-19 00:39:26 +0200

    my first commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+# print('hello am learning pyhton')
+# name = input('what is your name? :')
+# print('hello your name is:' + name)
+# print(f'hello your name is:{name}')
+n =int(input('enter any number :')) 
+if n > 0:
+    print('the number You entered is Positive')
+elif  n < 0:
+     print('the number You entered is Negative')
+
+else:
+      print('the number You entered is ZERO')
